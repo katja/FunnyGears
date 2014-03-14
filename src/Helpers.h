75f7873433abee4342dbd6eeaa7ef157f617b4fb
katja <ich-bin-sowieso@abwesend.de>
2014-03-14 12:34:53 +0100

    Added few new methods to Helpers.h

--- src/Helpers.h
+++ src/Helpers.h
@@ -1,16 +1,33 @@
 #ifndef HELPERS
 #define HELPERS
 
-std::ostream& operator<<(std::ostream& os, const Point& point) {
+#include "definitions.h"
+#include "Point.h"
+
+std::ostream& operator<<(std::ostream &os, const Point &point) {
     return os << "(" << point.pos().x() << ", " << point.pos().y() << ")";
 }
 
-std::ostream& operator<<(std::ostream& os, const QPointF& point) {
+std::ostream& operator<<(std::ostream &os, const QPointF &point) {
     return os << "(" << point.x() << ", " << point.y() << ")";
 }
 
-std::ostream& operator<<(std::ostream& os, const QRectF& rect) {
+std::ostream& operator<<(std::ostream &os, const QRectF &rect) {
     return os << "" << rect.topLeft() << ", " << rect.bottomRight() << "";
+}
+
+std::ostream& operator <<(std::ostream &os, const vec2 vec) {
+    return os << "[" << vec.x() << "| " << vec.y() << "]";
+}
+
+std::ostream& operator <<(std::ostream &os, const QVector<vec2> vector) {
+    os << "[";
+    int i = 0;
+    for(; i < vector.size() - 1; ++i) {
+        os << vector.at(i) << "|\t";
+    }
+    os << vector.at(i) << "]";
+    return os;
 }
 
 #endif // HELPERS
